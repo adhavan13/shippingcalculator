f0a92a8d8d0090957dba37446a91b04c44052510
Adhavan Se V <152024175+adhavan13@users.noreply.github.com>
2024-09-27 18:17:09 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,5 +1,6 @@
 weight=float(input("Enter the weight"))
 rate=float(input("Enter the cost"))
+additional=float(input("Enter the addition cost))
 
-total_price=weight*rate
+total_price=weight*rate+additional
 print(f"the total cost is{total_price} USD")
